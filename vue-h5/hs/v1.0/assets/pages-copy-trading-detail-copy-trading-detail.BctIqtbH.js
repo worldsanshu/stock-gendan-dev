6faa110a59fa1e87691569cd966ee261cf6e5f1c
tizi <manager_tizikeji@yeah.net>
2024-08-01 20:38:20 +0800

    修改问题

---
+++ vue-h5/hs/v1.0/assets/pages-copy-trading-detail-copy-trading-detail.BctIqtbH.js
@@ -0,0 +1,1 @@
+import{y as a,z as e,C as t}from"./index-BlcziP-N.js";import{_ as s}from"./_plugin-vue_export-helper.BCo6x5W8.js";const o=s(Object.assign({name:"copy-trading-detail"},{__name:"copy-trading-detail",setup:s=>(s,o)=>{const r=t;return a(),e(r,{class:"root"})}}),[["__scopeId","data-v-47cab52e"]]);export{o as default};
