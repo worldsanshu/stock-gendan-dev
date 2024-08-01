c9acd1dc1c13c9e9798b3ff13abe9e00f64deed9
tizi <manager_tizikeji@yeah.net>
2024-08-01 10:06:38 +0800

    修改问题

---
+++ vue-h5/fy/v1.0/assets/pages-index-margin-trading-index-margin-trading.CA42Pfss.js
@@ -0,0 +1,1 @@
+import{y as e,z as r,C as a}from"./index-v5N4FCa5.js";import{_ as s}from"./_plugin-vue_export-helper.BCo6x5W8.js";const n=s(Object.assign({name:"index-margin-trading"},{__name:"index-margin-trading",props:{index:{type:Number,required:!0}},setup:s=>(s,n)=>{const t=a;return e(),r(t,{class:"root"})}}),[["__scopeId","data-v-3a18b675"]]);export{n as default};
