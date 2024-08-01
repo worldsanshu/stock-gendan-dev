a03095e4c8cd8b23b1053c1be93a95970a1ddd3b
tizi <manager_tizikeji@yeah.net>
2024-08-01 20:27:14 +0800

    修改问题

---
+++ vue-h5/futu/assets/pages-copy-trading-contract-grid-copy-trading-contract-grid.DEFMzskM.js
@@ -0,0 +1,1 @@
+import{y as t,z as a,C as r}from"./index-Cy3uvPIr.js";import{_ as o}from"./_plugin-vue_export-helper.BCo6x5W8.js";const s=o(Object.assign({name:"copy-trading-contract-grid"},{__name:"copy-trading-contract-grid",setup:o=>(o,s)=>{const e=r;return t(),a(e,{class:"root"})}}),[["__scopeId","data-v-b74652d4"]]);export{s as default};
---
+++ vue-h5/futu/v1.0/assets/pz.VMwJRNjL.js
@@ -0,0 +1,1 @@
+import{aX as a,aY as o}from"./index-Cy3uvPIr.js";const s=()=>a("apicom/stock/day"),c=()=>a("apicom/stock/week"),p=()=>a("apicom/stock/month"),e=a=>o("apicom/handle/applySave",a),t=a=>o("apicom/handle/applySaveSub",a),i=o=>a("stock_detail",o);export{c as a,p as b,t as c,i as d,s as g,e as h};
