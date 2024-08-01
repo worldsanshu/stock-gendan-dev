be59e33594b5f2036b904cb1e384f5f8e85ad398
tizi <manager_tizikeji@yeah.net>
2024-08-01 19:49:19 +0800

    修改问题

---
+++ vue-h5/js/assets/pages-article-article.BRUUw0CN.js
@@ -0,0 +1,1 @@
+import{y as e,z as s,C as a}from"./index-MyvEfzqB.js";import{_ as t}from"./_plugin-vue_export-helper.BCo6x5W8.js";const r=t(Object.assign({name:"article"},{__name:"article",setup:t=>(t,r)=>{const o=a;return e(),s(o,{class:"root"})}}),[["__scopeId","data-v-f5642631"]]);export{r as default};
