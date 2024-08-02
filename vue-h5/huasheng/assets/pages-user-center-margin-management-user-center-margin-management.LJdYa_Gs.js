559d9c876772e68cda25d5c0bb1f3277a7710312
tizi <manager_tizikeji@yeah.net>
2024-08-02 14:50:38 +0800

    修改问题

---
+++ vue-h5/huasheng/assets/pages-user-center-margin-management-user-center-margin-management.LJdYa_Gs.js
@@ -0,0 +1,1 @@
+import{y as e,z as a,C as s}from"./index-DHIuICwK.js";import{_ as n}from"./_plugin-vue_export-helper.BCo6x5W8.js";const r=n(Object.assign({name:"user-center-margin-management"},{__name:"user-center-margin-management",setup:n=>(n,r)=>{const t=s;return e(),a(t,{class:"root"})}}),[["__scopeId","data-v-23b54f50"]]);export{r as default};
