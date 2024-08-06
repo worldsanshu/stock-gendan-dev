e35dc31ce1e0cd8fed0889ee1fcea63f3f57ce04
tizi <manager_tizikeji@yeah.net>
2024-08-06 19:26:36 +0800

    修该问题

---
+++ vue-h5/js/v1.0.4/assets/pz.tf3u144h.js
@@ -0,0 +1,1 @@
+import{bk as a,bl as o}from"./index-C6SCDi5e.js";const s=()=>a("apicom/stock/day"),c=()=>a("apicom/stock/week"),p=()=>a("apicom/stock/month"),e=a=>o("apicom/handle/applySave",a),t=a=>o("apicom/handle/applySaveSub",a),i=o=>a("stock_detail",o);export{c as a,p as b,t as c,i as d,s as g,e as h};
