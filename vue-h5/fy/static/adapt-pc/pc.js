7cf6682fd0d13fba2849f259af3d5d485a725154
tizi <manager_tizikeji@yeah.net>
2024-08-07 10:01:50 +0800

    修改问题

--- vue-h5/fy/static/adapt-pc/pc.js
+++
@@ -1,9 +0,0 @@
-;(function () {
-  if (window.innerWidth < 768) {
-    return
-  }
-  var tpl = document.querySelector('#tpl-adapt-pc').innerHTML || ''
-  tpl = tpl.replace('helang-mobile-href', window.location.href)
-  document.querySelector('uni-adapt-pc').innerHTML = tpl
-  document.body.setAttribute('adapt', 'pc')
-})()
